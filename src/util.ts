956d910911b9c4872f0419642bfbe655651b35f7
LunaChevalier <luna.chevalier.817.sk@gmail.com>
2020-09-29 14:19:42 +0900

    不要なメソッドを削除

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,3 @@
-export function getTitle(headers: string[]) {
-  const title = headers.find((header) => /^title:/.test(header)) || "non title";
-  return title.replace(/^title: /, "").replace(/^"/, "").replace(/"$/, "");
-}
-
-export function getEmoji(headers: string[]) {
-  const emoji = headers.find((header) => /^emoji:/.test(header)) || "🈳";
-  return emoji.replace(/^emoji: /, "").replace(/^"/, "").replace(/"$/, "");
-}
-
 export function getHeader(text: string) {
   const newLineHex = /\r\n|\n/;
   const separate = "---";
